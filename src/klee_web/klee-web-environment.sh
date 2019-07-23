b590ec0d4aa4564ed3772938727c00fbb26d83a6
Denis Gavrielov <de.gavrielov@gmail.com>
2019-07-23 17:23:06 +0100

    make python tests work

--- src/klee_web/klee-web-environment.sh
+++ src/klee_web/klee-web-environment.sh
@@ -1,8 +1,8 @@
-export REDIS_HOST=localhost
+export REDIS_HOST=kleeweb-redis
 export REDIS_DB_NUMBER=0
 export REDIS_PORT=6379
 
-export CELERY_BROKER_URL=redis://localhost:6379/0
+export CELERY_BROKER_URL=redis://kleeweb-redis:6379/0
 
 export DB_USER=klee_web
 export DB_PASSWORD=development
