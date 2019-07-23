64d2b27999495507d129ad03444e995727d4ab19
Denis Gavrielov <de.gavrielov@gmail.com>
2019-07-23 21:51:08 +0100

    test the e2e container in circle ci

--- src/klee_web/tests/run_tests.sh
+++ src/klee_web/tests/run_tests.sh
@@ -2,6 +2,9 @@
 
 . src/klee_web/tests/klee-web-environment.sh 
 
+# solve nodejs OPENSSL issues
+export OPENSSL_CONF=
+
 _Box () {
     str="$@"
     len=$((${#str}+4))
@@ -29,8 +32,8 @@ _Run "Running Flake8 against Python Code" "flake8 --ignore=E722 --max-complexity
 
 _Run "Running Python Unit tests" "(cd /titb/src/klee_web && python -m unittest discover -s worker/tests/ -p 'test_*.py')"
 
-_Run "Waiting for webserver to come up" "sleep 10"
+# _Run "Waiting for webserver to come up" "sleep 10"
 
 _Run "Checking that loading the homepage returns 200 OK" "http --check-status http://localhost/ > /dev/null"
 
-_Run "Running E2E webdriver tests" "cd /titb/src/klee_web/tests/ && npm test"
+# _Run "Running E2E webdriver tests" "cd /titb/src/klee_web/tests/ && npm test"
