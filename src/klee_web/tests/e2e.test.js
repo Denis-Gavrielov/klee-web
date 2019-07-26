51813008517550c427925c52daf3d20833bffd95
Denis Gavrielov <de.gavrielov@gmail.com>
2019-07-26 16:20:32 +0100

    js tests in own container

--- src/klee_web/tests/e2e.test.js
+++ src/klee_web/tests/e2e.test.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer');
-
 // // run: "node google.test.js" with the below to see the browser come up.
 // (async () => {
 //   const browser = await puppeteer.launch({
@@ -38,18 +37,45 @@ const puppeteer = require('puppeteer');
 //   await browser.close();
 // })();
 
+describe('Input', () => {
+  beforeAll(async () => {
+    // const page = await browser.newPage();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    await page.goto('http://192.168.33.10/');
+    // await page.screenshot({ path: 'one.png'})
+    await page.click('[ng-repeat="file in files"]');  // select first file ... use page.select instead?
+
+    await page.click('#run-klee-btn');
+    await page.waitFor('#result-output');
+    await browser.close();
+  });
+  it('tests that input files are processed correctly', async () => {
+
+    const text = await page.evaluate(() => document.querySelector('#result-output').innerText);
+    // this could actually open a file etc.
+    await expect(text).toMatch('KLEE: output directory is "/tmp/code/klee-out-0"\n' + 
+      'KLEE: Using STP solver backend\n\n' + 'KLEE: done: total instructions = 32\n' +
+      'KLEE: done: completed paths = 3\n' +'KLEE: done: generated tests = 3');
+    
+  });
+});
+
+
 
 describe('Admin', () => {
   beforeAll(async () => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     // const page = await browser.newPage();
     await page.goto('http://192.168.33.10/admin');
     await page.type('#id_username', 'admin');
     await page.type('#id_password', 'development');
     await page.click('[type=submit]');
+    await browser.close();
   });
   it('tests that the admin can login', async () => {
     var title = await page.title();
     await expect(title).toMatch("Site administration | Django site admin");
+    
   });
 });
 
@@ -60,6 +86,7 @@ describe('New Projects', () => {
   //   await expect(content).not.toMatch("Add New Project");
   // })
   beforeAll(async () => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     // const page = await browser.newPage();
     // var content = await page.content();
     // await page.goto('http://192.168.33.10/');
@@ -72,11 +99,13 @@ describe('New Projects', () => {
     await page.waitForNavigation();
     // console.log(page);
     // await page.screenshx§ot({ path: 'google.png'});
+    await browser.close();
   });
   it('tests that logged users can add new projects', async () => {
     // console.log(page);
     var content = await page.content();
     // console.log(content);
     await expect(content).toMatch("Add New Project");
+    
   });
 });
